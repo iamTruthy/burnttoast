c899c6265cb34380ad87bfbc6b36b774555cddc2
youfoundjerry <jerryjoekaizer@gmail.com>
2023-09-22 19:22:39 +0100

    initial commit

---
+++ Exercise1/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+var g int
+var w string = "#FIWB, Feed the pigs!"
+
+func main() {
+
+	message := "Sep 21, 2023. Moonbirds didn't moon so, they're now 'birds' "
+	fmt.Println(message)
+
+	man, number, NFT, _ := "Kevindegods", 33.3, "Degods", "y00ts"
+	fmt.Println(man, number, NFT)
+    
+	fmt.Println(w)
+	fmt.Println(g)
+
+	sayHello("Jerry")
+}
