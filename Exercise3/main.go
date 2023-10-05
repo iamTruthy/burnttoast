1505e68e1c6c1b7b7bbbd2fb90e72ba1102cd59b
youfoundjerry <jerryjoekaizer@gmail.com>
2023-10-06 00:47:44 +0100

    n++

--- Exercise3/main.go
+++ Exercise3/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 
-	a := 15
+	// a := 15
 	z := 9
 
 	for i := 0; i < 42; i++ {
@@ -27,7 +27,11 @@ func main() {
 		z++
 	}
 
-	// To show Modulus
+	/*
+	the correct code to print out ODD NUMBERS using the 
+	`continue` statement.
+	*/
+
 	for b := 0; b < 100; b++ {
 		if b%2 != 1 {
 			continue
@@ -35,12 +39,27 @@ func main() {
 		fmt.Println(b)
 	}
 
-	// An infinite Loop
-	for {
-		fmt.Printf("a is now %v\n", a)
-		if a > 50 {
+
+	/* 
+	another way to print out ODD NUMBERS 
+	but without the `continue` statement (used by the instructor)
+	*/ 
+	for n := 0; n < 100; n++ {
+		if n%2 != 0 {
+			fmt.Println(n)
 		}
-		a++
+
 	}
+
+
+
+
+	//An infinite Loop
+	// for {
+	// fmt.Printf("a is now %v\n", a)
+	// if a > 50 {
+	// }
+	// a++
+	// }
 
 }
