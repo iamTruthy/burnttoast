f7c48dd25666225a9cfa815ab4294c2f618d1454
youfoundjerry <jerryjoekaizer@gmail.com>
2023-10-31 13:23:18 +0100

    nothing here

--- Type-Constraint/Generics/main.go
+++ Type-Constraint/Generics/main.go
@@ -10,6 +10,8 @@ func addFloat(a, b float64) float64 {
 	return a + b
 }
 
+
+//This here, is a type constraint.
 func addIF[T int | float64](a, b T) T {
 	return a + b
 
@@ -17,10 +19,10 @@ func addIF[T int | float64](a, b T) T {
 
 func main() {
 
-	fmt.Println(addFloat(6.5,9.3))
-	fmt.Println(addInt(6,9))
+	fmt.Println(addFloat(6.5, 9.3))
+	fmt.Println(addInt(6, 9))
 
-	fmt.Println(addIF(3.4,7.3))
-	fmt.Println(addIF(6,2))
+	fmt.Println(addIF(3.4, 7.3))
+	fmt.Println(addIF(6, 2))
 
 }
