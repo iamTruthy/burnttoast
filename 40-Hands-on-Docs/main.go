adf4f8353f4354128f9f7f619ade97c11a56c58c
youfoundjerry <jerryjoekaizer@gmail.com>
2023-11-21 02:43:46 +0100

    documentation added

--- 40-Hands-on-Docs/main.go
+++ 40-Hands-on-Docs/main.go
@@ -1,12 +1,22 @@
 package main
 
+// Package dog is imported
 import (
 	"fmt"
 	"github.com/iamTruthy/dog"
 )
 
 func main() {
+// Package dog is called in func main and assigned to a variable human. 
+// it takes in a value of type int which is 
+/*
 
+func Years(a int) int {
+
+	return (a * bingo)
+}
+
+*/
 	human := dog.Years(2)
 	fmt.Println(human)
 
