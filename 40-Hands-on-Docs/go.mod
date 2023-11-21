8a283f503c135993b244199f204afaa72fd8bdbe
youfoundjerry <jerryjoekaizer@gmail.com>
2023-11-21 02:30:18 +0100

    documentation ongoing

---
+++ 40-Hands-on-Docs/go.mod
@@ -0,0 +1,5 @@
+module Documentation
+
+go 1.21.1
+
+require github.com/iamTruthy/dog v0.0.0-20231121011103-f163ccc6b1d7 // direct
---
+++ 40-Hands-on-Docs/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"github.com/iamTruthy/dog"
+)
+
+func main() {
+
+	human := dog.Years(2)
+	fmt.Println(human)
+
+}
